31853257800df3e952b6bcf0aea6ac578ecaf9bc
MSI-90 <104754352+MSI-90@users.noreply.github.com>
2024-02-25 17:05:32 +0700

    new method is added into BaseTest.java

--- src/test/java/tests/Base/BaseTest.java
+++ src/test/java/tests/Base/BaseTest.java
@@ -5,10 +5,16 @@ import Pages.AddCustomer.BankManagerPage;
 import Pages.Base.BasePage;
 import Pages.Customers.CustomersPage;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
 
 public class BaseTest {
     protected WebDriver driver = CommonAction.createDriver();
     protected BasePage basePage = new BasePage(driver);
     protected BankManagerPage bankCreatePage = new BankManagerPage(driver);
     protected CustomersPage customerPage = new CustomersPage(driver);
+
+    @AfterSuite(alwaysRun = true)
+    public void quit(){
+        driver.quit();
+    }
 }
